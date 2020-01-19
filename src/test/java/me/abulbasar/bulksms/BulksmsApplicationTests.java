0a1bbad614c6d01f8fca0bd8f03c2c83f96b865f
Bashar <basharkhan6@gmail.com>
2020-01-19 19:19:25 +0600

    Commit From NetBeans

---
+++ src/test/java/me/abulbasar/bulksms/BulksmsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.abulbasar.bulksms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulksmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
